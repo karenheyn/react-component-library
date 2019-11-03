ba8edc17b4ffeb5a98e24932b6dc1de6f7ef5202
karenheyn <karenheyn@gmail.com>
2019-11-03 03:32:38 -0500

    added stars

--- src/components/ProductThumbnails/Thumbnail.js
+++ src/components/ProductThumbnails/Thumbnail.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './Thumbnail.css'
 import mac from './mac.png'
+import rate from './rate.png'
 
 class Thumbnail extends Component {
     constructor(props) {
@@ -11,8 +12,8 @@ class Thumbnail extends Component {
         return ( 
             <div>
                 <div><img src = {mac} alt = 'mac'></img></div>
-                <hr></hr><h2>fvd</h2><img src = {mac} alt = 'mac'></img>
-                <div><h3>fdvd</h3></div>
+                <hr></hr><h2>fvd</h2><img src = {rate} alt = 'rating'></img>
+                <div><h3>$499</h3></div>
             </div>
          );
     }
