ec514e9de96271aa30f4ac4998be0a73aece2d64
karenheyn <karenheyn@gmail.com>
2019-11-03 01:22:55 -0500

    first checkbox working

--- src/components/Checkboxes/Checkbox.js
+++ src/components/Checkboxes/Checkbox.js
@@ -1,20 +1,32 @@
 import React, {Component} from 'react';
-// import checkmark from './checkmark'
+import './Checkbox.css'
+
  class Checkbox extends Component {
      constructor(props) {
          super(props);
          this.state = { 
-             checked: this.props.checked
+             isChecked: 'unchecked'
           }
+          this.handleCheckboxChange = this.handleCheckboxChange.bind(this)
      }
+     handleCheckboxChange = (event) =>{
+     if (this.state.isChecked === 'unchecked'){
+         this.setState({isChecked:'checked'})}
+        if (this.state.isChecked === 'checked'){
+            this.setState({isChecked:'unchecked'})}
+     }
+    
+    // this.setState({ checked: event.target.checked })
      render() { 
          return ( 
-            
-            <label className='container' for={this.props.name}>
+            <label 
+            className={this.state.isChecked}>
             <input 
             type='checkbox'
-            className='checkmark'
-            name = {this.props.name}></input>
+            className= {this.state.isChecked}
+            name = {this.props.name}
+            onChange = {this.handleCheckboxChange}>
+            </input>
             <span className='checkmark'></span>
             {this.props.text}</label>
           );
